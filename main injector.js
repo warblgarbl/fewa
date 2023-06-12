d1191ce5efcafdb83e1231adc5aacfb1d918bdca
warblgarbl <136350269+warblgarbl@users.noreply.github.com>
2023-06-12 12:35:05 -0400

    File pathing
    Restructured directory for .min files

--- main injector.js
+++ main injector.js
@@ -1,19 +1,21 @@
 var url = document.URL;
-var src = 'https://warblgarbl.github.io/fewa/';
+var min = '.min';
+var src = 'https://warblgarbl.github.io/fewa/script/' + min + min ? '\/' : '';
+var ext = min + '.js';
 
 if (/.*cic\.meridianlink\.com.*/.test(url)) {
 	src += 'cic\/';
 	if (/.*ClientConsumer\.aspx/.test(url)) {
-		src += 'Form.js';
+		src += 'Form' + ext;
 	} else if (/.*reports\/print_htm\.aspx/.test(url)) {
-		src += 'Report.js';
+		src += 'Report' + ext;
 	}
 } else if (/.*portal\.aquafinance\.com.*/.test(url)) {
 	src += 'aqua\/';
 	if (/.*applications\/?$/.test(url)) {
-		src += 'Dealer.js';
+		src += 'Dealer' + ext;
 	} else if (/.*applications\/new/.test(url)) {
-		src += 'NewApplication.js';
+		src += 'NewApplication' + ext;
 	}
 }
 
