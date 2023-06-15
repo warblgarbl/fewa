e1d9122e5a5be83cb985a4017c5f0caa2c075bc7
warblgarbl <136350269+warblgarbl@users.noreply.github.com>
2023-06-15 14:24:05 -0400

    One-click merged reports

--- script/cic/View_File.js
+++ script/cic/View_File.js
@@ -3,29 +3,47 @@ if (!$) {
 }
 
 $(document).ready(() => {
+	var $li = $('<li>');
+	var $inp = $('<input>');
+	var $lab = $('<label>');
+	$inp.attr({
+		'id': "fewa_bc",
+		'type': "checkbox",
+		'name': "fewa_bc",
+		'disabled': true
+	});
+	$lab.html('BOTH').attr({
+		'for': "fewa_bc"
+	});
+	$li.append($inp, $lab);
+	
 	var $labApp = $('label[for="custom_pc"]');
 	var $labCo = $('label[for="custom_sc"]');
 	
-	$labApp.wrapInner('<a href="javascript:void(0);" onclick="appOnly();"></a>');
-	$labCo.wrapInner('<a href="javascript:void(0);" onclick="coOnly();"></a>');
+	$labApp.parent('li').before($li);
+	$lab.wrapInner('<a href="javascript:void(0);" onclick="only(0);"></a>');
+	$labApp.wrapInner('<a href="javascript:void(0);" onclick="only(1);"></a>');
+	$labCo.wrapInner('<a href="javascript:void(0);" onclick="only(2);"></a>');
 });
 
-function appOnly() {
-	if (!$('#custom_pc:checked').length) {
-		$('#custom_pc').click();
+function only(n) {
+	var $app = $('#custom_pc');
+	var $co = $('#custom_sc');
+	
+	switch (parseInt(n)) {
+		case 0: {
+			if ($app.filter(':checked').length === 0) $app.click();
+			if ($co.filter(':checked').length === 0) $co.click();
+		} break;
+		case 1: {
+			if ($app.filter(':checked').length === 0) $app.click();
+			if ($co.filter(':checked').length !== 0) $co.click();
+		} break;
+		case 2: {
+			if ($app.filter(':checked').length !== 0) $app.click();
+			if ($co.filter(':checked').length === 0) $co.click();
+		} break;
 	}
-	if ($('#custom_sc:checked').length) {
-		$('#custom_sc').click();
-	}
-	$('#btnCustom').click();
-}
-
-function coOnly() {
-	if ($('#custom_pc:checked').length) {
-		$('#custom_pc').click();
-	}
-	if (!$('#custom_sc:checked').length) {
-		$('#custom_sc').click();
-	}
+	
 	$('#btnCustom').click();
 }
