0e63e2c1b084b1c5a7dca2b8a3e20a5a95e03df9
warblgarbl <136350269+warblgarbl@users.noreply.github.com>
2023-06-18 02:05:58 -0400

    sessionStorage to global var

--- script/cic/Report.js
+++ script/cic/Report.js
@@ -2,63 +2,77 @@ if (!$) {
 	var $ = jQuery;
 }
 
-var $alert = $('<audio>').attr({
-    'id': "fewaAlert",
-    'src': "https://docs.google.com/uc?export=download&id=149sQQRlfyVDEJ_KpJFJ8KBw_qGfsmsYX",
-    'paused': true
-  });
-$('body').children().eq(0).before($alert);
+var $fewaAlert = $('<audio>').attr({
+  'id': "fewaAlert",
+  'src': "https://docs.google.com/uc?export=download&id=149sQQRlfyVDEJ_KpJFJ8KBw_qGfsmsYX",
+  'paused': true
+});
+var fewaBop = 'wait';
+$('body').children().eq(0).before($fewaAlert);
+var $fewaLoad = $('<div>').html('Loading...').attr({
+  'id': "fewaLoad",
+  'style': "text-align:center;"
+});
+
+$('tbody').filter((i, e) => $(e).prev('thead').find('td.mcl2-report-section-header').filter((a, b) => {
+    let $b = $(b);
+    let id = b.innerHTML.split(' ')[0];
+    if (/(OPEN|CLOSED|DEROGATORY)/.test(id)) {
+      $b.attr('id', id);
+      $b.next().attr('style', 'text-align:center;').append($('<a>').attr({'href': "#SUMMARY"}).html('Back to summary'));
+      return 1;
+    } else return 0;
+  }).length).eq(0).parent('table').parent('span').before($('<span>')
+    .append($('<table>').attr({'style': 'width:100%;border-collapse:collapse;', 'cellspacing': '0', 'cellpadding': '0'})
+      .append($('<thead>')
+        .append($('<tr>')
+          .append($('<td>').attr('style', 'border-collapse: separate;')
+            .append($('div.mcl2-section-content-space').eq(0).clone())
+            .append($('<table>').attr({'cellspacing': '0', 'class': 'mcl12-table-ie-compatibility', 'align': 'center', 'style': 'width:100%;vertical-align:middle;border-bottom-style:solid;border-bottom-width:1px;border-bottom-color:Black;'})
+              .append($('<colgroup>')
+                .append($('<col>').attr('style', 'width:12%'))
+                .append($('<col>').attr('style', 'width:76%'))
+                .append($('<col>').attr('style', 'width:12%')))
+              .append($('<tbody>')
+                .append($('<tr>')
+                  .append($('<td>'))
+                  .append($('<td>').attr({'class': 'mcl2-report-section-header', 'style': 'padding:2px'}).html('SUMMARY'))
+                  .append($('<td>').attr('style', 'text-align:right;')))))
+            .append($('div.mcl2-section-content-space').eq(0).clone()))))
+      .append($fewaLoad)));
+
 
 $(document).on('click.f keydown.f', function() {
-	if (JSON.parse(sessionStorage.getItem('alert')) === true) {
-		$('#fewaAlert').trigger('play');
+	if (fewaBop === true) {
+		$fewaAlert.trigger('play');
 	}
-	if (JSON.parse(sessionStorage.getItem('alert')) !== 'wait') {
+	if (fewaBop !== 'wait') {
 		$(this).off('.f');
 	}
 })
 .ready(() => {
-	sessionStorage.setItem('alert', 'wait');
-	var sub = $('td.mcl2-report-body').filter((i, e) => /\*{3}-\*{2}/.test(e.innerHTML));
-	var rep = $('td.mcl2-report-body').filter((i, e) => /\*{5}/.test(e.innerHTML));
-	for (i = 0; i < sub.length; i++) {
-		if (sub[i].innerHTML.substr(-4) != rep[i].innerHTML.substr(-4)) {
-			$(sub[i]).parent().attr('style', 'background-color:palevioletred');
-			$(rep[i]).attr('style', 'background-color:lightgreen;' + $(rep[i]).attr('style'));
-			sessionStorage.setItem('alert', true);
+	var $subs = $('td.mcl2-report-body').filter((i, e) => /\*{3}-\*{2}/.test(e.innerHTML));
+	var $reps = $('td.mcl2-report-body').filter((i, e) => /\*{5}/.test(e.innerHTML));
+	for (let i = 0; i < $subs.length; i++) {
+    let $sub = $subs.eq(i);
+    let $rep = $reps.eq(i);
+		if ($sub.html().substr(-4) != $rep.html().substr(-4)) {
+			$sub.parent().attr('style', 'background-color:palevioletred');
+			$rep.attr('style', 'background-color:lightgreen;' + $rep.attr('style'));
+			fewaBop = true;
 		}
 	}
-	if (sessionStorage.getItem('alert') === 'wait') {
-		sessionStorage.setItem('alert', false);
-	}
-	
-  if ($('#fewaAlert').length) {
-    console.log('bingo!');
-  } else {
-     var $alert = $('<audio>').attr({
-       'id': "fewaAlert",
-       'src': "https://docs.google.com/uc?export=download&id=149sQQRlfyVDEJ_KpJFJ8KBw_qGfsmsYX",
-       'paused': true
-     });
-     $('body').children().eq(0).before($alert);
+  if (fewaBop !== true) {
+    fewaBop = false;
   }
 	
 	$('td').filter((i,e) => e.innerHTML === 'SCORE MODELS').attr('id', 'SUMMARY');
-	var $tbody = $('tbody').filter((i, e) => $(e).prev('thead').find('td.mcl2-report-section-header').filter((a, b) => {
-		let $b = $(b);
-		let id = b.innerHTML.split(' ')[0];
-		if (/(OPEN|CLOSED|DEROGATORY)/.test(id)) {
-			$b.attr('id', id);
-			$b.next().attr('style', 'text-align:center;').html('<a href="#SUMMARY">Back to summary</a>');
-			return 1;
-		} else return 0;
-	}).length);
+  var $tbody = $('tbody').filter((i, e) => $(e).prev('thead').find('td.mcl2-report-section-header').filter((a, b) => /^(OPEN|CLOSED|DEROGATORY)/.test(b.innerHTML)).length);
 	var $open = $tbody.eq(0).find('tr td table tbody');
 	var $closed = $tbody.eq(1).find('tr td table tbody');
 	var $derog = $tbody.eq(2).find('tr td table tbody');
 	
 	var $body = $('<tbody>');
-	var $space = $('<div>').attr('class', 'mcl2-section-content-space');
 	var $label = $('<div>').attr('class', 'mcl2-report-label');
 	var $table = $('<table>').attr({
 		'cellspacing': '0',
@@ -67,32 +81,14 @@ $(document).on('click.f keydown.f', function() {
 		'style': 'vertical-align:middle;'
 	});
 	
-	$tbody.eq(0).parent('table').parent('span').before($('<span>')
-		.append($('<table>').attr({'style': 'width:100%;border-collapse:collapse;', 'cellspacing': '0', 'cellpadding': '0'})
-			.append($('<thead>')
-				.append($('<tr>')
-					.append($('<td>').attr('style', 'border-collapse: separate;')
-						.append($space.clone())
-						.append($('<table>').attr({'cellspacing': '0', 'class': 'mcl12-table-ie-compatibility', 'align': 'center', 'style': 'width:100%;vertical-align:middle;border-bottom-style:solid;border-bottom-width:1px;border-bottom-color:Black;'})
-							.append($('<colgroup>')
-								.append($('<col>').attr('style', 'width:12%'))
-								.append($('<col>').attr('style', 'width:76%'))
-								.append($('<col>').attr('style', 'width:12%')))
-							.append($('<tbody>')
-								.append($('<tr>')
-									.append($('<td>'))
-									.append($('<td>').attr({'class': 'mcl2-report-section-header', 'style': 'padding:2px'}).html('SUMMARY'))
-									.append($('<td>').attr('style', 'text-align:right;')))))
-						.append($space.clone()))))
-			.append($body)));
 	
 	var title = ['ACCT TYPE', '#', 'BALANCE', 'HI CREDIT', 'PAYMENT', 'PAST DUE', '30 days', '60 days', '90+ days'];
 	var col = [];
-	for (i = 0; i < title.length; i++) {
+	for (let i = 0; i < title.length; i++) {
 		title[i] = $('<td>').attr('class', 'mcl2-report-label').html(title[i]);
 		switch (i) {
-			case 0: col.push($('<col>').attr('style', 'width:10%;')); break;
-			case 1: col.push($('<col>').attr('style', 'width:2%;')); break;
+			case 0: col.push($('<col>').attr('style', 'width:8%;')); break;
+			case 1: col.push($('<col>').attr('style', 'width:4%;')); break;
 			case 6: case 7: col.push($('<col>').attr('style', 'width:6%;')); break;
 			case 8: col.push($('<col>').attr('style', 'width:8%;')); break;
 			default: col.push($('<col>').attr('style', 'width:17%;'));
@@ -106,7 +102,7 @@ $(document).on('click.f keydown.f', function() {
 	var num = new RegExp(/\d+/);
 	var usd = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', maximumFractionDigits: 0});
 	var section = [$open, $closed, $derog];
-	for (a = 0; a < section.length; a++) {
+	for (let a = 0; a < section.length; a++) {
 		let sec = section[a];
 		if (sec.eq(0).children().length > 1) {
 			let $t = $table.clone();
@@ -143,32 +139,31 @@ $(document).on('click.f keydown.f', function() {
 			
 			let types = [mtg, auto, edu, rev, inst, _open, leas, coll, other];
 			let names = ['mtg', 'auto', 'edu', 'rev', 'inst', 'open', 'leas', 'coll', 'other'];
-			for (b = 0; b < types.length; b++) {
-				let row = {
-					'bal': 0,
-					'hi': 0,
-					'pay': 0,
-					'pd': 0,
-					'pd30': 0,
-					'pd60': 0,
-					'pd90': 0
-				}
-				
-				let accts = types[b];
-				row['type'] = names[b];
-				row['count'] = accts.length;
-				if (accts.length) {
-					for (c = 0; c < accts.length; c++) {
-						let $acct = $(accts[c]);
-						let row1 = $acct.children('tr').eq(1).children('td');
-						let row2 = $acct.children('tr').eq(2).children('td');
+			for (let b = 0; b < types.length; b++) {
+				if (types[b].length) {
+          let accts = types[b];
+          let row = {
+            'type': names[b].toUpperCase(),
+            'count': accts.length,
+            'bal': 0,
+            'hi': 0,
+            'pay': 0,
+            'pd': 0,
+            'pd30': 0,
+            'pd60': 0,
+            'pd90': 0
+          }
+					for (let c = 0; c < accts.length; c++) {
+						let acct = accts.eq(c);
+						let row1 = acct.children('tr').eq(1).children('td');
+						let row2 = acct.children('tr').eq(2).children('td');
+						let bal = row2.eq(3).text();
 						let hi = row1.eq(4).text();
+						let pd = row2.eq(4).text();
 						let pay = row1.eq(5).text();
 						let pd30 = row1.eq(6).text().split('30')[1];
 						let pd60 = row1.eq(7).text().split('60')[1];
 						let pd90 = row1.eq(8).text().split('90')[1];
-						let bal = row2.eq(3).text();
-						let pd = row2.eq(4).text();
 						
 						row.bal += num.test(bal) ? parseInt(num.exec(bal)) : 0;
 						row.hi += num.test(hi) ? parseInt(num.exec(hi)) : 0;
@@ -178,7 +173,7 @@ $(document).on('click.f keydown.f', function() {
 						row.pd60 += num.test(pd60) ? parseInt(num.exec(pd60)) : 0;
 						row.pd90 += num.test(pd90) ? parseInt(num.exec(pd90)) : 0;
 					}
-					for (key in row) {
+					for (let key in row) {
 						if (!/type/.test(key)) {
 							total[key] += row[key];
 							if (!/(\d|count)/.test(key)) {
@@ -186,7 +181,7 @@ $(document).on('click.f keydown.f', function() {
 							}
 						}
 					}
-					$b.append($('<tr style="text-align:center;">')
+					$b.append($('<tr>').attr({'style': "text-align:center;"})
 						.append($('<td>').html(row.type))
 						.append($('<td>').html(row.count))
 						.append($('<td>').html(row.bal))
@@ -198,16 +193,16 @@ $(document).on('click.f keydown.f', function() {
 						.append($('<td>').html(row.pd90)));
 				}
 			}
-			let head = $t.find('thead tr td');
-			for (key in total) {
-				if (!/(\d|count)/.test(key)) {
+			let $head = $t.find('thead tr td');
+			for (let key in total) {
+				if (!/(\d|count|type)/.test(key)) {
 					total[key] = usd.format(total[key]);
 				}
 			}
-			for (b = 0; b < head.length; b++) {
-				let $h = $(head[b]);
+			for (let b = 0; b < $head.length; b++) {
+				let $h = $head.eq(b);
 				let $l = $label.clone();
-				$l.html(head[b].innerHTML);
+				$l.html($h.html());
 				$h.empty().append($l);
 				switch (b) {
 					case 0: $h.append(total.type); break;
@@ -221,7 +216,8 @@ $(document).on('click.f keydown.f', function() {
 					case 8: $h.append(total.pd90);
 				}
 			}
-			$body.append($t.append($b)).append($space.clone());
+			$body.append($t.append($b)).append($('div.mcl2-section-content-space').eq(0).clone());
 		}
 	}
+  $fewaLoad.hide().after($body);
 });
