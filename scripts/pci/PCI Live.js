f4c04eb6bc6b9367c9407905905d9bcf43654e25
warblgarbl <136350269+warblgarbl@users.noreply.github.com>
2023-12-08 20:30:23 -0500

    PCI Decision Codes Updated: Codes are converted based on application date going back to July 27 2020. Fixed: PCI Live codes would only convert on first page of results.

--- scripts/pci/PCI Live.js
+++ scripts/pci/PCI Live.js
@@ -1,6 +1,8 @@
 const storage = chrome.storage.sync;
 
-$(document).ready(() => {
+$(document).on({
+  click: decisionCodeConversion
+}, "li.paginate_button > a").ready(() => {
   storage.get().then(result => {
     var pref = result.preferences.pci;
     if (pref.decCode) {
@@ -8,56 +10,7 @@ $(document).ready(() => {
         mutations.forEach(mutationRecord => {
           if (mutationRecord.target.style.display != "none")
             return;
-          var $head = $('.datatable thead th');
-          var dec;
-          for (let a = 0; a < $head.length; a++)
-            if (/Dec Code/i.test($head.eq(a).html())) {
-              dec = a;
-              break;
-            }
-          if (dec === undefined)
-            return;
-
-          var $rows = $('.datatable tbody tr[role=row]');
-          for (let i = 0; i < $rows.length; i++) {
-            var $row = $rows.eq(i);
-            var $child = $row.children();
-            var $dec = $child.eq(dec);
-            var $html = $dec.html();
-            switch ($html) {
-              case "A+":
-                $dec.html($html += ": 100%");
-                break;
-              case "A":
-                $dec.html($html += ": 98%");
-                break;
-              case "B":
-                $dec.html($html += ": 93%");
-                break;
-              case "C":
-                $dec.html($html += ": 88%");
-                break;
-              case "D":
-                $dec.html($html += ": 83%");
-                break;
-              case "E":
-                $dec.html($html += ": 78%");
-                break;
-              case "G":
-                $dec.html($html += ": 73%");
-                break;
-              case "H":
-                $dec.html($html += ": 68%");
-                break;
-              case "I":
-                $dec.html($html += ": 63%");
-                break;
-              case "J":
-                $dec.html($html += ": 58%");
-                break;
-            }
-          }
-          window.dispatchEvent(new Event('resize'));
+          decisionCodeConversion();
         });
       });
       let target = $('#loading-table').get()[0];
@@ -68,3 +21,163 @@ $(document).ready(() => {
     }
   })
 });
+
+function decisionCodeConversion() {
+  var $head = $('.datatable thead th');
+  var ind = {
+    date: "",
+    dec: ""
+  }
+  for (let a = 0; a < $head.length; a++) {
+    if (/Date/i.test($head.eq(a).html()))
+      ind.date = a;
+    if (/Dec Code/i.test($head.eq(a).html()))
+      ind.dec = a;
+  }
+  for (let key in ind)
+    if (ind[key] === "")
+      return;
+
+  var $rows = $('.datatable tbody tr[role=row]');
+  for (let i = 0; i < $rows.length; i++) {
+    var $row = $rows.eq(i);
+    var $child = $row.children();
+    var $date = $child.eq(ind.date);
+    var $dec = $child.eq(ind.dec);
+    var arrDate = $date.html().split("\/");
+    var date = new Date(Number(arrDate[2]), Number(arrDate[0]) - 1, Number(arrDate[1]));
+    var strDec = $dec.html();
+    if (!/\d/.test(strDec))
+      if (date > new Date(2020, 6, 26) && date < new Date(2021, 0, 24))
+        switch (strDec) {
+          case "A+":
+            $dec.html(strDec += ": <b>100%</b>");
+            break;
+          case "A":
+            $dec.html(strDec += ": <b>95%</b>");
+            break;
+          case "B":
+            $dec.html(strDec += ": <b>90%</b>");
+            break;
+          case "C":
+            $dec.html(strDec += ": <b>85%</b>");
+            break;
+          case "D":
+            $dec.html(strDec += ": <b>80%</b>");
+            break;
+          case "E":
+            $dec.html(strDec += ": <b>75%</b>");
+            break;
+          case "G":
+            $dec.html(strDec += ": <b>70%</b>");
+            break;
+          case "H":
+            $dec.html(strDec += ": <b>65%</b>");
+            break;
+          case "I":
+            $dec.html(strDec += ": <b>60%</b>");
+            break;
+          case "J":
+            $dec.html(strDec += ": <b>55%</b>");
+            break;
+        }
+    else if (date >= new Date(2021, 0, 24) && date < new Date(2023, 0, 27))
+      switch (strDec) {
+        case "A+":
+        case "A":
+          $dec.html(strDec += ": <b>100%</b>");
+          break;
+        case "B":
+          $dec.html(strDec += ": <b>95%</b>");
+          break;
+        case "C":
+          $dec.html(strDec += ": <b>90%</b>");
+          break;
+        case "D":
+          $dec.html(strDec += ": <b>85%</b>");
+          break;
+        case "E":
+          $dec.html(strDec += ": <b>80%</b>");
+          break;
+        case "G":
+          $dec.html(strDec += ": <b>75%</b>");
+          break;
+        case "H":
+          $dec.html(strDec += ": <b>70%</b>");
+          break;
+        case "I":
+          $dec.html(strDec += ": <b>65%</b>");
+          break;
+        case "J":
+          $dec.html(strDec += ": <b>60%</b>");
+          break;
+      }
+    else if (date >= new Date(2023, 0, 27) && date < new Date(2024, 0, 2))
+      switch (strDec) {
+        case "A+":
+          $dec.html(strDec += ": <b>100%</b>");
+          break;
+        case "A":
+          $dec.html(strDec += ": <b>98%</b>");
+          break;
+        case "D":
+          $dec.html(strDec += ": <b>83%</b>");
+          break;
+        case "C":
+          $dec.html(strDec += ": <b>88%</b>");
+          break;
+        case "B":
+          $dec.html(strDec += ": <b>93%</b>");
+          break;
+        case "E":
+          $dec.html(strDec += ": <b>78%</b>");
+          break;
+        case "G":
+          $dec.html(strDec += ": <b>73%</b>");
+          break;
+        case "H":
+          $dec.html(strDec += ": <b>68%</b>");
+          break;
+        case "I":
+          $dec.html(strDec += ": <b>63%</b>");
+          break;
+        case "J":
+          $dec.html(strDec += ": <b>58%</b>");
+          break;
+      }
+    else if (date >= new Date(2024, 0, 2))
+      switch (strDec) {
+        case "A+":
+          $dec.html(strDec += ": <b>100%</b>");
+          break;
+        case "A":
+          $dec.html(strDec += ": <b>95%</b>");
+          break;
+        case "B":
+          $dec.html(strDec += ": <b>90%</b>");
+          break;
+        case "C":
+          $dec.html(strDec += ": <b>85%</b>");
+          break;
+        case "D":
+          $dec.html(strDec += ": <b>80%</b>");
+          break;
+        case "E":
+          $dec.html(strDec += ": <b>75%</b>");
+          break;
+        case "G":
+          $dec.html(strDec += ": <b>70%</b>");
+          break;
+        case "H":
+          $dec.html(strDec += ": <b>65%</b>");
+          break;
+        case "I":
+          $dec.html(strDec += ": <b>60%</b>");
+          break;
+        case "J":
+          $dec.html(strDec += ": <b>55%</b>");
+          break;
+      }
+  }
+  window.dispatchEvent(new Event('resize'));
+}
--- scripts/pci/STAReport.js
+++ scripts/pci/STAReport.js
@@ -14,8 +14,8 @@ $(document).ready(() => {
             id: `#fewa-pcistar-${tar}-refresh`,
             class: buttons.children('button').eq(0).attr('class')
           });
-          buttons.append(btn.on('click', null, tar, decision).append($('<span>').html("Decision %")));
-          decision(null, tar);
+          buttons.append(btn.on('click', null, tar, decisionCodeConversion).append($('<span>').html("Decision %")));
+          decisionCodeConversion(null, tar);
         });
       });
     }
@@ -30,13 +30,13 @@ $(document).ready(() => {
   })
 });
 
-function decision(e, tar) {
+function decisionCodeConversion(e, tar) {
   var $head = $(`#stareport-${tar}-datatable thead th`);
   var ind = {
     date: "",
     rep: "",
     dec: ""
-  };
+  }
   for (let a = 0; a < $head.length; a++) {
     let head = $head.eq(a).html();
     if (/Date/i.test(head))
@@ -46,6 +46,11 @@ function decision(e, tar) {
     if (/Dec.*Code/i.test(head))
       ind.dec = a;
   }
+  for (let key in ind)
+    if (ind[key] === "")
+      return;
+  var strDate;
+  var arrDate;
   var date;
   var swap = 1;
   var shade = ["#0069a633", "#f38b0044"];
@@ -56,54 +61,157 @@ function decision(e, tar) {
     let $date = $child.eq(ind.date);
     let $rep = $child.eq(ind.rep);
     let $dec = $child.eq(ind.dec);
-    if (window.pref.report) {
-      if (date != $date.html()) {
-        date = $date.html();
-        switch (swap) {
-          case 0:
-            swap = 1;
-            break;
-          case 1:
-            swap = 0;
-            break;
-        }
+    let strRep = $rep.html();
+    let strDec = $dec.html();
+    if (strDate != $date.html()) {
+      strDate = $date.html();
+      arrDate = strDate.split("\/");
+      date = new Date(Number(arrDate[2]), Number(arrDate[0]) - 1, Number(arrDate[1]));
+      switch (swap) {
+        case 0:
+          swap = 1;
+          break;
+        case 1:
+          swap = 0;
+          break;
       }
-      $row.attr({ style: "background-color:" + shade[swap] });
     }
-    if (/TIM/i.test($rep.html()))
+    if (window.pref.report)
+      $row.attr({ style: "background-color:" + shade[swap] });
+    if (/TIM/i.test(strRep))
       $rep.empty();
     if (window.pref.decCode)
-      switch ($dec.html()) {
-        case "A+":
-          $dec.html($dec.html() + ": <b>100%</b>");
-          break;
-        case "A":
-          $dec.html($dec.html() + ": <b>98%</b>");
-          break;
-        case "D":
-          $dec.html($dec.html() + ": <b>83%</b>");
-          break;
-        case "C":
-          $dec.html($dec.html() + ": <b>88%</b>");
-          break;
-        case "B":
-          $dec.html($dec.html() + ": <b>93%</b>");
-          break;
-        case "E":
-          $dec.html($dec.html() + ": <b>78%</b>");
-          break;
-        case "G":
-          $dec.html($dec.html() + ": <b>73%</b>");
-          break;
-        case "H":
-          $dec.html($dec.html() + ": <b>68%</b>");
-          break;
-        case "I":
-          $dec.html($dec.html() + ": <b>63%</b>");
-          break;
-        case "J":
-          $dec.html($dec.html() + ": <b>58%</b>");
-          break;
+      if (!/\d/.test(strDec)) {
+        if (date > new Date(2020, 6, 26) && date < new Date(2021, 0, 24))
+          switch (strDec) {
+            case "A+":
+              $dec.html(strDec += ": <b>100%</b>");
+              break;
+            case "A":
+              $dec.html(strDec += ": <b>95%</b>");
+              break;
+            case "B":
+              $dec.html(strDec += ": <b>90%</b>");
+              break;
+            case "C":
+              $dec.html(strDec += ": <b>85%</b>");
+              break;
+            case "D":
+              $dec.html(strDec += ": <b>80%</b>");
+              break;
+            case "E":
+              $dec.html(strDec += ": <b>75%</b>");
+              break;
+            case "G":
+              $dec.html(strDec += ": <b>70%</b>");
+              break;
+            case "H":
+              $dec.html(strDec += ": <b>65%</b>");
+              break;
+            case "I":
+              $dec.html(strDec += ": <b>60%</b>");
+              break;
+            case "J":
+              $dec.html(strDec += ": <b>55%</b>");
+              break;
+          }
+        else if (date >= new Date(2021, 0, 24) && date < new Date(2023, 0, 27))
+          switch (strDec) {
+            case "A+":
+            case "A":
+              $dec.html(strDec += ": <b>100%</b>");
+              break;
+            case "B":
+              $dec.html(strDec += ": <b>95%</b>");
+              break;
+            case "C":
+              $dec.html(strDec += ": <b>90%</b>");
+              break;
+            case "D":
+              $dec.html(strDec += ": <b>85%</b>");
+              break;
+            case "E":
+              $dec.html(strDec += ": <b>80%</b>");
+              break;
+            case "G":
+              $dec.html(strDec += ": <b>75%</b>");
+              break;
+            case "H":
+              $dec.html(strDec += ": <b>70%</b>");
+              break;
+            case "I":
+              $dec.html(strDec += ": <b>65%</b>");
+              break;
+            case "J":
+              $dec.html(strDec += ": <b>60%</b>");
+              break;
+          }
+        else if (date >= new Date(2023, 0, 27) && date < new Date(2024, 0, 2))
+          switch (strDec) {
+            case "A+":
+              $dec.html(strDec += ": <b>100%</b>");
+              break;
+            case "A":
+              $dec.html(strDec += ": <b>98%</b>");
+              break;
+            case "D":
+              $dec.html(strDec += ": <b>83%</b>");
+              break;
+            case "C":
+              $dec.html(strDec += ": <b>88%</b>");
+              break;
+            case "B":
+              $dec.html(strDec += ": <b>93%</b>");
+              break;
+            case "E":
+              $dec.html(strDec += ": <b>78%</b>");
+              break;
+            case "G":
+              $dec.html(strDec += ": <b>73%</b>");
+              break;
+            case "H":
+              $dec.html(strDec += ": <b>68%</b>");
+              break;
+            case "I":
+              $dec.html(strDec += ": <b>63%</b>");
+              break;
+            case "J":
+              $dec.html(strDec += ": <b>58%</b>");
+              break;
+          }
+        else if (date >= new Date(2024, 0, 2))
+          switch (strDec) {
+            case "A+":
+              $dec.html(strDec += ": <b>100%</b>");
+              break;
+            case "A":
+              $dec.html(strDec += ": <b>95%</b>");
+              break;
+            case "B":
+              $dec.html(strDec += ": <b>90%</b>");
+              break;
+            case "C":
+              $dec.html(strDec += ": <b>85%</b>");
+              break;
+            case "D":
+              $dec.html(strDec += ": <b>80%</b>");
+              break;
+            case "E":
+              $dec.html(strDec += ": <b>75%</b>");
+              break;
+            case "G":
+              $dec.html(strDec += ": <b>70%</b>");
+              break;
+            case "H":
+              $dec.html(strDec += ": <b>65%</b>");
+              break;
+            case "I":
+              $dec.html(strDec += ": <b>60%</b>");
+              break;
+            case "J":
+              $dec.html(strDec += ": <b>55%</b>");
+              break;
+          }
       }
   }
   window.dispatchEvent(new Event('resize'));
