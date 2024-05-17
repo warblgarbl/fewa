d14a782c85e275d231ceca682e0ed59b783aded0
warblgarbl <136350269+warblgarbl@users.noreply.github.com>
2024-05-17 01:33:32 -0400

    Salesforce Dashboard Auto-refresh time adjusted. Added toggle button.

--- scripts/salesforce/Dashboard.js
+++ scripts/salesforce/Dashboard.js
@@ -1,8 +1,10 @@
-window.dashInitiate = undefined;
-window.dashRefresh = undefined;
+var $window = {};
+$window.dashInitiate = undefined;
+$window.dashRefresh = undefined;
+$window.dashInterval = 1000 * 60 * 10;
 
 function getLastRefresh() {
-  var lastRefresh = $('span.lastRefreshDate').eq(0).html().split('As of ')[1].split(', ');
+  var lastRefresh = $('span.lastRefreshDate').html().split('As of ')[1].split(', '); // [MMM d, YYYY, H:mm a]
   lastRefresh = {
     date: {
       month: lastRefresh[0].split(' ')[0],
@@ -15,6 +17,7 @@ function getLastRefresh() {
       pm: /PM/i.test(lastRefresh[2])
     }
   }
+  // Convert MMM string to 0-index
   switch (lastRefresh.date.month) {
     case 'January':
       lastRefresh.date.month = 0;
@@ -53,10 +56,12 @@ function getLastRefresh() {
       lastRefresh.date.month = 11;
       break;
   }
-  if (lastRefresh.time.hour == 12 && !lastRefresh.time.pm)
+  // Convert 12-hour to 24-hour
+  if (lastRefresh.time.hour == 12 && !lastRefresh.time.pm) // midnight hour
     lastRefresh.time.hour = 0;
-  else if (lastRefresh.time.hour < 12 && lastRefresh.time.pm)
+  else if (lastRefresh.time.hour < 12 && lastRefresh.time.pm) // afternoon hours
     lastRefresh.time.hour += 12;
+
   return new Date(
     lastRefresh.date.year,
     lastRefresh.date.month,
@@ -67,27 +72,49 @@ function getLastRefresh() {
 
 function refreshCycle() {
   var now = new Date();
+  // Do not run during normal business hours.
   if (now.getHours() < 7 || now.getHours() > 17)
     return;
-  var refresh = $('button.refresh').eq(0);
+  var refresh = $('button.refresh');
   var prevRefresh = getLastRefresh();
-  if ((now.getTime() - prevRefresh.getTime()) > (1000 * 60 * 4)) {
+  // Only refresh if at least 5 minutes have passed since last refresh
+  if ((now.getTime() - prevRefresh.getTime()) > ($window.dashInterval - 1000)) {
     refresh.click();
     setTimeout(() => {
+      // If refresh was unsuccessful, refresh the iframe
       if (prevRefresh.getTime() == getLastRefresh().getTime())
         document.location.reload();
-    }, 1000 * 20);
+    }, 1000 * 10);
   }
 }
 
 $(document).ready(() => {
   var observer = new MutationObserver(() => {
-    if ($('button.refresh').length > 0 && this.dashInitiate == undefined) {
-      this.dashInitiate = setTimeout(() => {
-        refreshCycle();
-        this.dashRefresh = setInterval(refreshCycle, 1000 * 60 * 5);
+    // Only run a single time, 10 seconds after refresh button is available
+    if ($('button.refresh').length > 0 && $window.dashInitiate === undefined) {
+      $window.dashInitiate = setTimeout(() => {
+        var autoRefresh = $('<button>').html('Auto-refresh').attr({
+          class: 'slds-button slds-button_neutral auto-refresh',
+          type: 'button'
+        });
+        $('button.refresh').after(autoRefresh);
+        autoRefresh.click();
       }, 1000 * 10);
     }
   })
   observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
-});
+}).on({
+  click: function () {
+    var $this = $(this);
+    if ($window.dashRefresh === undefined) {
+      $this.addClass('refreshing');
+      refreshCycle();
+      $window.dashRefresh = setInterval(refreshCycle, $window.dashInterval);
+    } else {
+      $this.removeClass('refreshing');
+      clearTimeout($window.dashInitiate);
+      clearInterval($window.dashRefresh);
+      $window.dashRefresh = undefined;
+    }
+  }
+}, 'button.auto-refresh');
